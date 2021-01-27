0881345fc13fb4b5a6cdc5437acd08c38449efd2
name-CrystalHo <cth6xmj@virginia.edu>
2021-01-26 19:03:50 -0500

    Added herokue

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +130,4 @@ STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+django_heroku.settings(locals())

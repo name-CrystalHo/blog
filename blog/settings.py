b5449569f3b57941a47713084d876ed318d4d4db
Crystal Ho <cth6xmj@virginia.edu>
2021-02-01 02:14:29 -0500

    added static files

--- blog/settings.py
+++ blog/settings.py
@@ -151,7 +151,7 @@ STATICFILESDIRS=[
     os.path.join(BASE_DIR,'static'),  
     '../theblog/static',
 ]
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+STATIC_ROOT='/static/'
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')

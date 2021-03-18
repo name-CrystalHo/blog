0c72660d1044d3906d335a1e72dcce96e6dbe1f2
name-CrystalHo <cth6xmj@virginia.edu>
2021-03-18 17:54:16 -0400

    collected static

--- blog/settings.py
+++ blog/settings.py
@@ -173,13 +173,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILESDIRS=[
-    os.path.join(BASE_DIR,'static'),  
-    '../theblog/static',
+    os.path.join(BASE_DIR,'staticfiles'),  
+    '../theblog/staticfiles',
 ]
-STATIC_ROOT='/static/'
+STATIC_ROOT='/staticfiles/'
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')

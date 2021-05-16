5461110b5addc04791f64665dd7fff995a55a8eb
name-CrystalHo <cth6xmj@virginia.edu>
2021-05-16 19:05:38 -0400

    changed aws

--- blog/settings.py
+++ blog/settings.py
@@ -20,9 +20,9 @@ SECRET_KEY = '6tzd&u@)aigf6p&nxhujvalb!h+uo8(i9-t8zc=$5dd(09#in('
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-AWS_ACCESS_KEY_ID = config('aws_id')
-AWS_SECRET_ACCESS_KEY = config('aws_secret')
-AWS_STORAGE_BUCKET_NAME =config('s3_name')
+# AWS_ACCESS_KEY_ID = config('aws_id')
+# AWS_SECRET_ACCESS_KEY = config('aws_secret')
+# AWS_STORAGE_BUCKET_NAME =config('s3_name')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'

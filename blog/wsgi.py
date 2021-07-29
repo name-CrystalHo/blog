c77453a06bac548bc37579b8adb3a11d6fcdef5d
name-CrystalHo <cth6xmj@virginia.edu>
2021-07-29 12:03:50 -0400

    Changed path

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
+
+sys.path.insert(0, '/opt/python/current/blog/')   # I use ElasticBeanstalk, modify to your project path
+

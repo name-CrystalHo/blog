b039dc4b8cbaa0ce5643cc7f5aacca371a6920aa
name-CrystalHo <cth6xmj@virginia.edu>
2021-08-26 15:36:53 -0400

    Added media files

--- blog/settings.py
+++ blog/settings.py
@@ -20,21 +20,21 @@ SECRET_KEY = '6tzd&u@)aigf6p&nxhujvalb!h+uo8(i9-t8zc=$5dd(09#in('
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-# if 'aws_id' in os.environ:
-#     AWS_ACCESS_KEY_ID = os.environ.get('aws_id')
-#     AWS_SECRET_ACCESS_KEY = os.environ.get('aws_secret')
-#     AWS_STORAGE_BUCKET_NAME = os.environ.get('s3_name')
-#     AWS_S3_FILE_OVERWRITE = False
-#     AWS_DEFAULT_ACL = None
-# else:
-#     AWS_ACCESS_KEY_ID = config('aws_id')
-#     AWS_SECRET_ACCESS_KEY = config('aws_secret')
-#     AWS_STORAGE_BUCKET_NAME =config('s3_name')
-#     AWS_S3_FILE_OVERWRITE = False
-#     AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# ADMIN_MEDIA_PREFIX = '/static/admin/' 
+if 'aws_id' in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ.get('aws_id')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('aws_secret')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('s3_name')
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+else:
+    AWS_ACCESS_KEY_ID = config('aws_id')
+    AWS_SECRET_ACCESS_KEY = config('aws_secret')
+    AWS_STORAGE_BUCKET_NAME =config('s3_name')
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+ADMIN_MEDIA_PREFIX = '/static/admin/' 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
